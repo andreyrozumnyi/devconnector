f186b3769a4200f51044bb3ca048105aa6a272ed
Andrii Rozumnyi <andrii.rozumnyi@pipedrive.com>
2019-12-05 18:42:50 +0200

    add components for individual profiles

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import AddEducation from './components/profile-forms/AddEducation';
 import AddExperience from './components/profile-forms/AddExperience';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 import setAuthToken from './utils/setAuthToken';
 // Redux
@@ -49,6 +50,11 @@ const App = () => {
                                 exact
                                 path='/profiles'
                                 component={Profiles}
+                            />
+                            <Route
+                                exact
+                                path='/profile/:id'
+                                component={Profile}
                             />
                             <PrivateRoute
                                 exact
